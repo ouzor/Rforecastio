e6e611125c8013d8cf14b7df721d6feb5e0c134a
ouzor <ouzor@iki.fi>
2014-11-02 09:58:58 +0200

    Added exclusion for currently,daily, minutely to make the call faster

--- R/Rforecastio.R
+++ R/Rforecastio.R
@@ -52,10 +52,10 @@ fio.forecast <- function(api.key, latitude, longitude, for.time, time.formatter=
 
   # using RCurl's getURLContent() since it fully supports http or https
   if (missing(for.time)) {
-    fio.json <- getURLContent(url=sprintf("https://api.forecast.io/forecast/%s/%s,%s/?units=si",
+    fio.json <- getURLContent(url=sprintf("https://api.forecast.io/forecast/%s/%s,%s/?units=si&exclude=currently,minutely,daily",
                                       api.key, latitude, longitude), ...)
   } else {
-    fio.json <- getURLContent(url=sprintf("https://api.forecast.io/forecast/%s/%s,%s,%d/?units=si",
+    fio.json <- getURLContent(url=sprintf("https://api.forecast.io/forecast/%s/%s,%s,%d/?units=si&exclude=currently,minutely,daily",
                                       api.key, latitude, longitude, for.time), ...)
   }
 
